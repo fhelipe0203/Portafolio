fc501f3b1e20e3a8ba9103ff8930fb57cb11ce3e
fhelipe0203 <92058233+fhelipe0203@users.noreply.github.com>
2022-08-12 18:47:44 -0300

    novo scripts

--- Octavio/scripts.js
+++ Octavio/scripts.js
@@ -6,9 +6,26 @@ btn.addEventListener("click", function(e) {
   
   const name = document.querySelector("#nome");
   
-  
   const value = name.value;
 
-  console.log(value);
+  let email = document.querySelector("#email");
+  
+  const  value2 = email.value;
+
+  let numero = document.querySelector("#numero");
+
+  const nume = numero.value;
+
+  let mensaje = document.querySelector("#mensaje")
+
+  const mensa = mensaje.value;
+
+ body = {
+      "nome": value,
+      "Correio": value2,
+      "celular": nume,
+      "mensajedetexto": mensa,
+       }
+   console.log(body)    
 });
 
